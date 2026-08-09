0d529d8664bd09772985f1e131a6ff5942365864
MADHU T <mtvipooo123@gmail.com>
2026-08-10 01:13:56 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,16 @@
+name = "Madhu" #string
+age = input("Enter your age: ") #int
+print(age)
+boy_name = input("Enter your boy name: ") #string
+boy_age = input("Enter your boy age: ") #int
+girl_name = input("Enter your girlfriend name: ") #string
+girl_age = input("Enter your girlfriend age: ") #int
+# using abs() function to find the absolute difference between boy and girl
+"""this 
+is a 
+multi line
+commet
+"""
+age_diff=abs(int(boy_age)-int(girl_age));
+print(boy_name + " loves " + girl_name +  " .age_difference is " + str(age_diff)+"years") 
+
