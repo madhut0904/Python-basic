1ec6f42dfb9a71f1acebd15a1a71919f08f63085
MADHU T <mtvipooo123@gmail.com>
2026-08-09 23:48:13 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+name = "Madhu" #string
+age =20 #int
+is_student = True #boolean
+weight = 65.5 #float
+print(type(name))
+x=100;
+print(int(x)+age)
---
+++ basic.py
@@ -0,0 +1,22 @@
+print("hello madhu")
+a=10
+b=20
+print(a);
+A,B,C=20,30,50;
+print(A+B+C)
+print(b//a)
+print(b%a)
+print(b**a)
+print(b*a)
+print(b/a)
+print(b-a)
+#swap two variables
+a,b=b,a;
+print(a,b)
+name = "Madhu" #string
+age =20 #int
+is_student = True #boolean
+weight = 65.5 #float
+print(type(name))
+x=100;
+print(int(x)+age)
