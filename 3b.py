c6cdc1eac30544b52c7bba12393e8e4511f9d65b
MADHU T <mtvipooo123@gmail.com>
2026-08-11 09:25:09 +0530

    Add files via upload

---
+++ 3b.py
@@ -0,0 +1,26 @@
+f_name = "Madhu"
+l_name = "Kumar"
+full_name = f_name + " " + l_name
+print(full_name)
+
+message = "Hello warning! "
+print(message*3)
+print(message.upper())
+print(message.lower())
+print(message.strip()*2)
+print(message.replace("warning","error")    )
+
+print(len(message))
+text = "Python"
+print(text[0])
+print(text[4])
+print(text[-1])
+print(text[0:4])
+print(text[2:])
+print(text[:4])
+print(text[::2])
+print(text[::-1])
+
+x = "Python\nprogramming is coding language"
+print(x)
+
