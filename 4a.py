203b4c65971a73921134ffcc4a2ca3c88623bcda
MADHU T <mtvipooo123@gmail.com>
2026-08-13 00:50:23 +0530

    Add files via upload

---
+++ 4a.py
@@ -0,0 +1,25 @@
+#operation in python 
+#assignment operator
+a=10
+print(a)
+a+=5
+print(a)
+a-=3
+print(a)
+a*=2
+print(a)
+a=a+5
+Print(a)
+a/=2
+print(a)
+
+
+#comparison operator
+a=10
+b=20
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
