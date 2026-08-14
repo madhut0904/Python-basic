e37c02151dc847795dece7035cce409bc8e89cd1
MADHU T <mtvipooo123@gmail.com>
2026-08-14 20:14:18 +0530

    Add files via upload

---
+++ 4b.py
@@ -0,0 +1,40 @@
+#operators
+#logical operators
+print(True and False)
+print(True or False)
+print(not(True))
+print(not(False))
+print(not(1))
+print(not(0))
+print(not(-1))
+print(True and True)
+print(True or True)
+print(False and False)
+print(False or False)
+
+print(1>2)
+print(1<2)
+print(1>2 and 2>1)
+print(1>2 or 2>1)
+
+
+#membership operators
+a = [1,2,3,4,5]
+print(1 in a)
+print(6 in a)
+print(1 not in a)
+print(6 not in a)
+x="hello"
+print("h" in x)
+print("z" in x)
+
+
+#bitwise operators
+a = 10
+b = 4
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
+print(a << 2)
+print(a >> 2)
