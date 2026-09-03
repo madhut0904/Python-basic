b3e2749b4e0cb6089a34e100112dc745d0f7f2d1
MADHU T <mtvipooo123@gmail.com>
2026-09-04 02:15:15 +0530

    Add age and marks eligibility checks

---
+++ 7.py
@@ -0,0 +1,17 @@
+age = 20
+
+if age >= 18:
+age = 16
+
+if age >= 18:
+    print("Eligible to vote.")
+else:
+    print("Not eligible to vote.")
+marks = 75
+
+if marks >= 90:
+    print("A Grade")
+elif marks >= 60:
+    print("B Grade")
+else:
+    print("C Grade")7
